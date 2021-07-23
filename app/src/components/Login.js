cd7f05c93914dffc63f4f6ad12a5b515bdb6b880
crivadeneyra.09@gmail.com <crivadeneyra.09@gmail.com>
2021-07-22 21:44:59 -0500

    Se crea state en comp login

--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -4,8 +4,33 @@ import '../style/Login.css'
 //import Leftimage from '../logo.svg'
 import Leftimage from '../image/LeftLogo.png'
 import Logo from '../image/R&R-Logo.png'
+
+
+
+
 class Login extends React.Component {
 
+
+    state = {
+        
+        user:'',
+        password: ''
+    }
+
+    handleChange = e => {
+
+        //cada input esta guardando su propio valo + setState
+        //console.log(e.target.value);
+        this.setState ({
+            [e.target.name]: e.target.value,
+        })
+
+    }
+    handleSubmit = e =>{
+
+        console.log(this.state)
+
+    }
     //Define el elemento que se vera en pantalla
     render() {
 
@@ -30,18 +55,38 @@ class Login extends React.Component {
                             <form className='form-login'>
 
                                 <div className='form-group'>
-                                    <label>Email</label>
-                                    <input className='form-control'></input>
+                                    <label>Usuario</label>
+                                    <input 
+                                    onChange={this.handleChange}
+                                    type='text'
+                                    autoComplete='username'
+                                    className='form-control' 
+                                    placeholder='Ingre se usuario'
+                                    name="user"
+                                    value={this.state.user}
+                                    />
 
                                 </div>
 
 
                                 <div className='form-group'>
                                     <label>Password</label>
-                                    <input className='form-control'></input>
+                                    <input 
+                                    onChange={this.handleChange}
+                                    type='password'
+                                    autoComplete='new-password'
+                                    className='form-control' 
+                                    placeholder='Ingre se password'
+                                    name='password'
+                                    value={this.state.password}
+                                    />
 
                                 </div>
-                                <button type='button' class='btn btn-primary'>Log in</button>
+                                <button 
+                                type='button' 
+                                className='btn btn-primary'
+                                onClick={this.handleSubmit}
+                                >Log in</button>
 
                                 <div className='form-footer'>
                                     <label>¿Olvidaste tu contraseña?</label>
