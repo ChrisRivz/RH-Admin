9c765ad0f73f0977e533d6f9dc3684e1a45ef19c
crivadeneyra.09@gmail.com <crivadeneyra.09@gmail.com>
2021-07-23 10:24:35 -0500

    Route react implenting in process

--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Login from './components/Login';
+//import App from './components/app'
+
 import 'bootstrap/dist/css/bootstrap.css'
 
 
