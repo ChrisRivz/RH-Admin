c7f46ea0cdb0989fac7633a3fac0a28b08ecab1f
crivadeneyra.09@gmail.com <crivadeneyra.09@gmail.com>
2021-07-27 20:53:51 -0500

    changes

--- app/src/components/Layout.js
+++ app/src/components/Layout.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Avatar from '../image/Avatars/User1_Avatar.jpg'
+import Logo from '../image/R&R-Logo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Dropdown from 'react-bootstrap/Dropdown';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 
-import '../style/Layout-header.css'
+import '../style/Menu.css'
 
 
 class Layout extends React.Component {
@@ -20,67 +24,71 @@ class Layout extends React.Component {
         return (
 
             <div>
-                <nav className="navbar navbar-expand-sm bg-light navbar-light">
-                    <div className="row">
-
-
-                        <div className='col-lg-6'>
-
-                        </div>
-
-                        <div className='col-lg-6'>
-
-
-                            <div className="navbar-contianer">
-
-                                <ul className="nav">
-                                    <li className="nav-item">
-                                        <label className="nav-link">Bienvenido Christopher Emanuel</label>
-                                    </li>
-                                    <li className="nav-item">
-
-                                        <span className="nav-item">
-
-                                            <Dropdown>
-                                                <Dropdown.Toggle id="dropdown-basic">
-
-                                                    <img src={Avatar} alt='Avatar' />
-                                                </Dropdown.Toggle>
-
-                                                <Dropdown.Menu>
-                                                    <Dropdown.Item href="#/action-1"><FontAwesomeIcon className='icon-user' icon={['fas', 'user-edit']} />Mi Perfil</Dropdown.Item>
-                                                    <Dropdown.Item href="#/action-2"><FontAwesomeIcon className='icon-logout' icon={['fas', 'sign-out-alt']} />Logout</Dropdown.Item>
-                                                </Dropdown.Menu>
-                                            </Dropdown>
-
-
-                                        </span>
-                                    </li>
-                                </ul>
-                            </div>
-
-                        </div>
-
-                    </div>
-                </nav>
-
 
                 <Row>
-                    <Col className='nav-menu' sm={2}>
+                    <Col className='nav-menu' sm={3}>
+                        <Row className="Header-menu">
+                            <Col sm={12}>
+                                <Nav className="justify-content-end" activeKey="/home">
+                                    <Nav.Item>
 
-                    <Container>
+                                        <Nav.Link href="/home"><FontAwesomeIcon className='icon-bell' icon={['far', 'bell']} /></Nav.Link>
 
-                        <Nav defaultActiveKey="/home" className="flex-column">
+                                    </Nav.Item>
+                                    <Nav.Item>
 
-                            <Nav.Link className='nav-link' href="/home"><FontAwesomeIcon className='icon-id' icon={['fas', 'id-card']} />Mi Perfil</Nav.Link>
-                            <Nav.Link className='nav-link' eventKey="link-1"><FontAwesomeIcon className='icon-file' icon={['fas', 'file-invoice']} />Recibos de nomina</Nav.Link>
-                     
-                        </Nav>
+                                        <Nav.Link href="/home"><FontAwesomeIcon className='icon-user' icon={['far', 'user-circle']} /></Nav.Link>
+                                    </Nav.Item>
+
+                                </Nav>
+                            </Col>
+
+                            <Col className="Section-profile" sm={12}>
+                                <Image className="Avatar-profile" src={Avatar} roundedCircle />
+                                <h6>
+                                    Christopher Rivadeneyra
+                                </h6>
+                                <label>christopher.rivadeneyra@itconsultingrr.com</label>
+                            </Col>
+
+                            <Col sm={12}>
+                                <ListGroup className='Menu-list'>
+                                    <ListGroup.Item> <FontAwesomeIcon className='icon-user-list' icon={['far', 'user-circle']} /> Perfil</ListGroup.Item>
+                                    <ListGroup.Item> <FontAwesomeIcon className='icon-receipt' icon={['fas', 'receipt']} /> Recibos de Nomina</ListGroup.Item>
+                                </ListGroup>
+                            </Col>
+                        </Row>
 
 
-                        </Container>
                     </Col>
-                    <Col sm={8}></Col>
+                    <Col sm={9}>
+
+                        <Navbar>
+                            <Container>
+                                <Navbar.Text href="#"><FontAwesomeIcon className="icon-menu" icon={["fas", "bars"]}></FontAwesomeIcon></Navbar.Text>
+                                <Navbar.Toggle />
+                                <Navbar.Collapse className="justify-content-end">
+                                    <Navbar.Text>
+                                        <a href="#login">Salir</a>
+                                    </Navbar.Text>
+                                </Navbar.Collapse>
+                            </Container>
+                        </Navbar>
+
+                        <Navbar className="Navbar-content">
+                            <Row>
+
+                                <Col sm={12}>
+                                    <h3>¡Bienvenido, Christopher!</h3></Col>
+                            </Row>
+                        </Navbar>
+
+
+
+                    </Col>
+                </Row>
+
+                <Row>
                 </Row>
 
 
